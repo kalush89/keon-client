1609230655a0805b38f9a2a5b4da546138c6caee
albertkombol <kombolalbert@gmail.com>
2023-11-10 08:19:27 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotificationListComponent } from './notification-list/notification-list.component';
+import { SettingsComponent } from './settings/settings.component';
+import { GoalListComponent } from './goal-list/goal-list.component';
+import { GoalComponent } from './goal/goal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MenuComponent,
+    NotificationListComponent,
+    SettingsComponent,
+    GoalListComponent,
+    GoalComponent
   ],
   imports: [
     BrowserModule,
